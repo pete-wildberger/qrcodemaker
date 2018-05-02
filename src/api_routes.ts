b18af177ba54f770840fb516bd7e22a50e0923fb
Peter Wildberger <pete.wildberger@gmail.com>
2018-05-01 20:10:37 -0500

    base api setup

---
+++ src/api_routes.ts
@@ -0,0 +1,6 @@
+import * as express from 'express';
+import * as path from 'path';
+
+const router = express.Router();
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,16 @@ import * as QRCode from 'qrcode';
 import * as express from 'express';
 import * as path from 'path';
 import * as bodyParser from 'body-parser';
+import api_routes from './api_routes';
 
 const app = express();
 const port = process.env.PORT || 8081;
-const router = express.Router();
 
 app.use(express.static('client/build'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use('/', )
 
-// app.get('/', (req, res) => {
-//   console.log('base url hit');
-//   QRCode.toDataURL('I am a pony!',  (err, url) => {
-//     console.log(url);
-//   })
-//   res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
+app.use('/api', api_routes);
 
 app.listen(port);
 
